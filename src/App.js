8e6cf44e5abfdd66adad7c60a6d7151fc0eca8bb
sanyamk0 <sanyamkumar002@gmail.com>
2023-10-11 22:13:45 +0530

    added login and signup components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
