5cff8f91103fb0f9a6fdf42a10560361a08b5dfe
sanyamk0 <sanyamkumar002@gmail.com>
2023-10-15 15:37:43 +0530

    fetched products, filtered and sorted using redux-toolkit(productSlice and productAPI)

--- src/features/product/ProductAPI.js
+++ src/features/product/ProductAPI.js
@@ -1,8 +1,24 @@
-export function fetchCount(amount = 1) {
-    return new Promise(async (resolve) => {
-      const response = await fetch("http://localhost:8080");
-      const data = await response.json();
-      resolve({ data });
-    });
+export function fetchAllProducts() {
+  //TODO: We will not hard-code server URL
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+export function fetchProductsByFilters(filter) {
+  // filter={"category":"smartphone"}
+  // TODO:On Server We will support multiple values
+  let queryString = "";
+  for (let key in filter) {
+    queryString += `${key}=${filter[key]}&`;
   }
-  
+  //ToDO: We will not hard-code server URL
+  return new Promise(async (resolve) => {
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await response.json();
+    resolve({ data });
+  });
+}
