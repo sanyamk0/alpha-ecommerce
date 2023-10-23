41fdcc3ce5d1df55203e9b7fc941450ced54a967
sanyamk0 <sanyamkumar002@gmail.com>
2023-10-24 02:21:37 +0530

    removed css file import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
